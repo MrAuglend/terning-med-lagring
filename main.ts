71ace7d4c7c99bfad5919b1bcbf6ac26e35e7fdd
MrAuglend <103996341+MrAuglend@users.noreply.github.com>
2022-06-10 09:53:57 +0200

    Klar, men ikke ordnet tabell. Kan også kobles opp mot radiomottak

--- main.ts
+++ main.ts
@@ -1,6 +1,23 @@
-input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 6))
+input.onButtonPressed(Button.A, function () {
+    terning = randint(1, 6)
+    basic.showString("Kast")
+    basic.showNumber(terning)
+    Antall_kast += 1
+    Sum_terningkast += terning
+    Gjennomsnitt = Sum_terningkast / Antall_kast
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Snitt")
+    basic.showNumber(Gjennomsnitt)
+})
+let terning = 0
+let Sum_terningkast = 0
+let Antall_kast = 0
+let Gjennomsnitt = 0
+Gjennomsnitt = 0
+Antall_kast = 0
+Sum_terningkast = 0
+terning = 0
 basic.forever(function () {
 	
 })
